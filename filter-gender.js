650f8294dfa8de187bdd99714c6f17e8e0f4ac4e
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-27 15:58:50 -0300

    filtrando array de objetos com o filter

---
+++ filter-gender.js
@@ -0,0 +1,14 @@
+const data = require("./database.js");
+
+function filterByGender(gender) {
+    if (gender === "M" || gender === "F") {
+        const result = data.characters.filter((character) => {
+            return character.gender === gender;
+        });
+        return result.length;
+    } else {
+        return "Valor inválido!";
+    }
+}
+
+console.log(filterByGender("F"));
